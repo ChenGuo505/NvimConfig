169d418fa965df4a907e0b96770643274da4bcfe
Chen Guo <guochen505@gmail.com>
2025-12-31 13:34:19 +0800

    config blink

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -7,10 +7,22 @@ return {
 		keymap = { preset = 'super-tab' },
 
 		appearance = {
-			nerd_font_variant = 'mono'
+			nerd_font_variant = 'normal'
 		},
 
-		completion = { documentation = { auto_show = false } },
+		completion = {
+			accept = { auto_brackets = { enabled = true } },
+			menu = {
+				border = "rounded"
+			},
+			documentation = {
+				auto_show = true,
+				auto_show_delay_ms = 200,
+				window = {
+					border = "rounded"
+				}
+			}
+		},
 
 		sources = {
 			default = { 'lsp', 'path', 'snippets', 'buffer' },
